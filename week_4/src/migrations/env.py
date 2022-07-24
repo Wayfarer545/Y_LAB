4a028602cdbbcfc7dcccc855dce6e81579ce20bb
Wayfarer <conversionGSX@gmail.com>
2022-07-24 11:19:16 +0300

    ".env added, db config changed"

--- week_4/src/migrations/env.py
+++ week_4/src/migrations/env.py
@@ -1,3 +1,4 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -7,11 +8,12 @@ from sqlmodel import SQLModel
 from alembic import context
 
 from src.models import Post, Account
-from src.core import DATABASE_URL
+from src.core import DATABASE_URL, PROJECT_NAME
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("script_location", os.path.join("src", "migrations"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
